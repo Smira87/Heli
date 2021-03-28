4899ae41711c19ea0d34f7b3c8544316ebe87879
Smirnov Sergiy <snoff87@gmail.com>
2021-03-28 19:55:49 +0300

    shoot script1

---
+++ Assets/Scripts/HeliShootScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeliShootScript : MonoBehaviour
+{
+
+    public Button rightFire;
+    public Button leftFire;
+    PhotonView view;
+    public GameObject Bullet;
+
+    public int force = 10;
+    
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+        GameObject rightFireButton = GameObject.Find("RightFireButton");
+        GameObject leftFireButton = GameObject.Find("LeftFireButton");
+        rightFire = rightFireButton.GetComponent<Button>(); 
+        leftFire = leftFireButton.GetComponent<Button>(); 
+        rightFire.onClick.AddListener(FireLeft);
+        leftFire.onClick.AddListener(FireRight);
+        
+    }
+ void FireLeft()
+    {
+         if (view.isMine)
+        {
+            view.RPC("shootBullet", PhotonTargets.All, transform.Find("ShootPosition").transform.position, transform.Find("ShootPosition").transform.rotation);
+        }
+    }
+void FireRight()
+    {
+         if (view.isMine)
+        {
+            view.RPC("shootBullet", PhotonTargets.All, transform.Find("ShootPosition1").transform.position, transform.Find("ShootPosition").transform.rotation);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [PunRPC]
+    void shootBullet(Vector3 Pos, Quaternion quaat)
+    {
+        GameObject GO = Instantiate(Bullet, Pos, quaat) as GameObject;
+        GO.GetComponent<Rigidbody>().velocity = transform.TransformDirection((Vector3.right * force));
+    }
+
+    
+}
